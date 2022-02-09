2867a5c2cd3b89482bb95b7370069569093b26cf
Roman Vlasov <rvlasov@telematika.com>
2022-02-09 21:57:53 +0300

    Add .env file

--- src/main.js
+++ src/main.js
@@ -2,19 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import {vuetify} from './plugins/vuetify'
 import {i18n} from './plugins/vue-i18n'
-import VuetifyToast from 'vuetify-toast-snackbar'
 import store from './store'
 
 Vue.config.productionTip = false
-
-Vue.use(VuetifyToast, {
-    shorts: {
-        custom: {
-            color: 'purple'
-        }
-    },
-    property: '$toast'
-});
 
 new Vue({
     vuetify,
--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,14 @@
 import axios from './axios'
 
-const API_KEY = '767647942a0f777311afe0714eee4be4'
-
-// const API_KEY ='767647942a0f777311afe0714eee4be4-'
+const API_KEY = process.env.VUE_APP_API_KEY
 
 export function getCityByLocationName(name) {
-    return axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=1&appid=${API_KEY}`)
+    return axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=1&appid=${API_KEY}`)
         .then(response => Promise.resolve(response.data.length ? response.data[0] : null));
 }
 
 export function getCurrentWeatherForLocation({lon, lat}, lang = 'en') {
-    return axios.get(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=${lang}`)
+    return axios.get(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=${lang}`)
         .then(response => Promise.resolve(response.data));
 }
 
